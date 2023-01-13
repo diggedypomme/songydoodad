147a266399beff7d1fb4a3c0a35f48cd2b2f0b8a
diggedypomme <superpomme@gmail.com>
2023-01-13 01:14:30 +0000

    Create mysuggestions.js

---
+++ js/suggestions/mysuggestions.js
@@ -0,0 +1,268 @@
+Currently not logged
+
+
+
+
+
+
+La Femme - Y tu te vas
+https://youtu.be/NSC_5GBmG5I
+
+
+Mon Laferte - Mi Buen Amor (En Vivo)
+https://youtu.be/xD27uAJUMIo
+
+Crystalites - Slippery
+https://youtu.be/ltuAllwMK7E
+
+Grutz - Listen Here [UK, Afro Rock/Jazz] (1973) Eddie Harris cover
+https://youtu.be/rwTzZLVw0TA
+
+Altin Gün - Süpürgesi Yoncadan (Live on KEXP)
+https://youtu.be/InymYroypxg
+
+Pentagram - 20 Buck Spin
+https://youtu.be/zKS5Tw7mlk4
+
+Maró Riba - Jinvunda
+https://youtu.be/2ehUY2uyovg
+
+DUB SPECIALIST - Bionic Dub [1977]
+https://youtu.be/0XYB71h-nls
+
+French 79 feat. Sarah Rebecca - Diamond Veins
+https://youtu.be/p-ZvHDaR72Q
+
+Barcelona Guitar Trio & Dance - Billie Jean (Homenaje a Paco de Lucía) New version
+https://youtu.be/olHI-y3bDaM
+
+Mary Sylvester - Light Of The World & Version (Sylvester's Inc. ) 1985
+https://youtu.be/vBAtg2MSYZQ
+
+The Gaylads - Over The Rainbows End
+https://youtu.be/1O420JHtdeQ
+
+Tinariwen - Aldhechen Manin (Foxall Edit)
+https://youtu.be/c76ELBbmk80
+
+Doreen Ketchens - House of the Rising Sun
+https://youtu.be/mjr8jZ83vic
+
+Funny Shadow
+https://youtu.be/fog3DVz6Bt4
+
+Decades (2007 Remaster)
+https://youtu.be/5-SZraSMiEc
+
+Mungo's Hi Fi, Marina P - Divorce A L'Italienne
+https://youtu.be/tX5jrH0e16I
+
+Luna Llena - Los Espíritus
+https://youtu.be/rfjHf6PTKAk
+
+Bessoso - Para Decir Que Te Quiero
+https://youtu.be/3fSCqqTDiQw
+
+Colonial Mentality
+https://youtu.be/zXKxB-J3hgI
+
+Vandal Moon - Robot Lover (FM Attack Remix)
+https://youtu.be/dhMnXih2hhQ
+
+Natalia Doco, Devendra Banhart - Quedate Luna (10 Ton Obsidian & Spin Alonga Rework)
+https://youtu.be/oRW4pSg64WY
+
+Overseer Sparks
+https://youtu.be/ra96yzjhr6o
+ 
+Into You by Richard X feat Jarvis Coker and Mazzy Star ...
+https://youtu.be/eIq6Msnxdhc
+
+Falta de respeto Carla Morrison (acústico)
+https://youtu.be/dsd9yfu-p8c
+
+Newen Afrobeat feat. Seun Kuti & Cheick Tidiane Seck - Opposite People (Fela Kuti)
+https://youtu.be/mFSRCG4DrmI
+
+ERROL WALKER - In These Times [1977]
+https://youtu.be/JREqY9EnD9U
+
+Black Box Recorder - Child Psychology
+https://youtu.be/rJjqEIam15Y
+
+Hush (2006 Digital Remaster)
+https://youtu.be/SazAz5W92EQ
+
+DELROY BUTLER - Different Experience
+https://youtu.be/7SmTIusLkI8
+
+Guts - Nosso Carimbó é do Mundo Feat. Pinduca & Nazaré Pereira 
+https://youtu.be/tq_EgUILKPk
+
+Aim - Demonique
+https://youtu.be/FXCYMDQHgOY
+
+French 79 - Hometown [Official Video]
+https://youtu.be/yAv5pLO37mE
+
+Nancy Sinatra - Don't Let Him Waste Your Time 
+https://youtu.be/judh99G3-rs
+
+Opa Cupa - Due inutili parole
+https://youtu.be/vWqTAmdyjOs
+
+roots manuva - witness the fitness
+https://youtu.be/NDWgtB_MD24
+
+The Fjords - All In
+https://youtu.be/ZxNfD1fIdzM
+
+Still Corners - Sad Movies
+https://youtu.be/FRm-l9SWi_0
+
+sol seppy Slo Fuzz
+https://youtu.be/wd27YI1Es3Q
+
+Sol Seppy - enter one
+https://youtu.be/ypApKTXuOxU
+
+Nouvelle Vague - In A Manner Of Speaking 
+https://youtu.be/zXhLFb34nz4
+
+Militant
+https://youtu.be/4jNwdss9LtQ
+
+Lila Downs - Zapata Se Queda 
+https://youtu.be/beuRgIqxTnY
+
+Zenzile - Pulse Weed (Tetra Hydro K remix)
+https://youtu.be/rIgDsd23yFQ
+
+Starálfur
+https://youtu.be/7fEUUnXDnbk
+
+Owen Gray - Give Me A Little Sign (Gimme Little Sign - Brenton Wood Cover)
+https://youtu.be/V9N2DEzQ208
+
+Gnonnas Pedro - Yiri Yiri Boum (audio)
+https://youtu.be/N26alCjCCEc
+
+Mazouni - Écoute-moi camarade
+https://youtu.be/mnsa9dazW04
+
+Cielo Azul
+https://youtu.be/nbT9XeM3ox0
+
+Zeigeist - Dawn/ /Night
+https://youtu.be/0u4XYQZyUyc
+
+La Yegros Viene De Mi Monkey Sessions
+https://youtu.be/gRS5-hr83DE
+
+Vivian Jackson & The Prophets - Anti Christ & King Tubby Dub
+https://youtu.be/Ck6rhtILgqY
+
+Delroy Wilson ‎– I'm Not A King (Studio One) 1968
+https://youtu.be/Fn46sAnkK9g
+
+Iseo & Dodosound - Chan Chan 
+https://youtu.be/DNCZEGJmHZo
+
+Ike Bennett & The Crystalites - Illya Kuryakin
+https://youtu.be/mZ0LeGMpVrw
+
+Tove Lo - Habits (Stay High) - Hippie Sabotage Remix
+https://youtu.be/SYM-RJwSGQ8
+
+Janet Kay - Silly games - Top of The Pops 1979
+https://youtu.be/qCVR5XR04Mo
+
+B.B. Seaton ‎– Morning Sun & Sundown (High Note) 1978
+https://youtu.be/5EfQew0e28E
+
+Ondubground x Panda Dub - Deep Frequency #animation
+https://youtu.be/cNDF9moEXtQ
+
+Ernest Ranglin - Surfin'
+https://youtu.be/NuyrY--vs3U
+
+Tinariwen (+IO:I) - Ténéré Tàqqàl (what has become of the Ténéré)
+https://youtu.be/boiiiVh52v4
+
+Et si on était tous riches - Manu Louis
+https://youtu.be/XSGIo98Z3L0
+
+Kanute - Not Sleeping
+https://youtu.be/cd3lvTmDo7U
+
+Errol Dunkley - Stop Your Lying Girl
+https://youtu.be/KAcHnX5JaTY
+
+The Upsetters - Return of Django 
+https://youtu.be/ax5phXqMjCU
+
+The nurse who loved me - A Perfect Circle
+https://youtu.be/CWRDZwV8jGE
+
+The Cables ‎– Jah Children Unite & Unite Version (Bright Star) 197?
+https://youtu.be/_jPuFOCRG0k
+
+40 Shades of Green
+https://youtu.be/MH8Op9WPM6Y
+
+The Paragons - "The Tide Is High" 
+https://youtu.be/SQXqkiKXiHc
+
+Love Theme---The Godfather (1972)---Nino Rota
+https://youtu.be/lSj9IdwLfzo
+
+Disrupt - Chip On Fire
+https://youtu.be/gXJiPszwkTo
+
+Guts - Aimer sans amour 
+https://youtu.be/T1ZNnkttFnc
+
+Well Pleased (Disco Mix)
+https://youtu.be/q6W5CK8B428
+
+Slagsmalsklubben - Sponsored by Destiny 
+https://youtu.be/5B9hPGyabaQ
+
+Alice - Tom Waits
+https://youtu.be/aEj-mrwwaxo
+
+Droga Do Ocalenia
+https://youtu.be/KtXgWLxDl8s
+
+Panda Dub - The Lost Ship - 5 - Planet pillow
+https://youtu.be/04b3bun5FoQ
+
+Derajah-jam
+https://youtu.be/ReVk93Klkuw
+
+Os Tincoãs - Deixa A Gira Girar (j g b edit)
+https://youtu.be/3xt8Mp8oWzc
+
+Home - We're Finally Landing
+https://youtu.be/zR6fECxF44I
+ 
+Flavia Coelho - Sunshine 
+https://youtu.be/1NjXMrhP_iE
+
+Angèle - La Thune
+https://youtu.be/m3YX8zlR4BU
+
+Giorgio Moroder - Tears (1972)
+https://youtu.be/edtKQBV-DSI
+
+Degiheugi - Kolkata [OFFICIAL VIDEO]
+https://youtu.be/QgbNr9nB84I
+
+Save a Bread
+https://youtu.be/XV6AozrexVI
+
+Yann Tiersen - Monochrome
+https://youtu.be/OKBB74nXPr4
+
+Flight of the Conchords - Father And Son
+https://youtu.be/QG_TBIGY2Iw
